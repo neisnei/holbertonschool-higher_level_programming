d117e546d25a08311dfc0b0faa1da625bf2cc84a
neisnei <neischalyruidiaz@gmail.com>
2023-09-28 00:28:00 +0000

    README.md

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-for ascii_value in range(97, 123):
-     print('{}'.format(chr(ascii_letters)), end="")
+for ascii_letters in range(97, 123):
+    if ascii_value == 101 or ascii_value == 113:
+        continue
+    print("{:c}".format(ascii_value), end="")
