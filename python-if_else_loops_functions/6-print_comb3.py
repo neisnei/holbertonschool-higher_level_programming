dde379b50951c6e06ed2e8fe626b61b4dc06011f
neisnei <neischalyruidiaz@gmail.com>
2023-09-28 01:00:04 +0000

    README.md

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
 for num1 in range(10):
-    for num2 in range(10):
-        if num1 < num2:
-            print("{}{}".format(num1, num2), end="")
-            if num1 < 8:
-                print(", ", end="")
+    for num2 in range(num1 + 1, 10):
+        print("{:02d}".format(num1), end="")
+        print("{:02d}".format(num2), end="")
+        if num1 != 8 or num2 != 9:
+            print(", ", end="")
+        else:
+            print()
