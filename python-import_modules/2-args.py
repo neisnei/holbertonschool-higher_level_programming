67e015d7f621716227dd5f22f2b190ab9b21d326
neisnei <neischalyruidiaz@gmail.com>
2023-09-28 02:00:45 +0000

    README.md

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    """Print the number of and list of arguments."""
+    import sys
 
-args = sys.argv[1:]
-num_args = len(args)
-
-print("Número de argumento(s):", num_args, end="")
-if num_args == 0:
-    print(".", end="")
-print("\n")
-
-if num_args > 0:
-    for i, arg in enumerate(args, start=1):
-        print("Argumento", i, ":", arg)
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
