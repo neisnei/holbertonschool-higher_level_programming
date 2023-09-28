a8e64411d5cf934708256275a821e7efead3b583
neisnei <neischalyruidiaz@gmail.com>
2023-09-28 01:02:50 +0000

    README.md

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-for num1 in range(10):
-    for num2 in range(num1 + 1, 10):
-        print("{:02d}".format(num1), end="")
-        print("{:02d}".format(num2), end="")
-        if num1 != 8 or num2 != 9:
-            print(", ", end="")
+for digit1 in range(0, 10):
+    for digit2 in range(digit1 + 1, 10):
+        if digit1 == 8 and digit2 == 9:
+            print("{}{}".format(digit1, digit2))
         else:
-            print()
+            print("{}{}".format(digit1, digit2), end=", ")
