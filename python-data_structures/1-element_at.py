f25e419c1ea9851480aec5db41beab6e5fa4c4fe
neisnei <neischalyruidiaz@gmail.com>
2023-10-03 23:11:23 +0000

    README.md

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -15,11 +15,3 @@ def safe_print_list(my_list=[], x=0):
         except IndexError:
             print()
             return i
-    print()
-    return i
-
-if __name__ == "__main__":
-    main()
-
-
-def main():
