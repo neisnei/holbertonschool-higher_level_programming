3db176942b349ef520fd99a5dee2bea9b053fdfe
neisnei <neischalyruidiaz@gmail.com>
2023-10-03 23:10:05 +0000

    README.md

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+def element_at(my_list, idx)
+    if idx < 0:
+        return None
+    elif idx > len(my_list):
+        return None
+    else:
+        return my_list[idx]
+
+
+def safe_print_list(my_list=[], x=0):
+    for i in range(0, x):
+        try:
+            print("{}".format(my_list[i]), end="")
+        except IndexError:
+            print()
+            return i
+    print()
+    return i
+
+if __name__ == "__main__":
+    main()
+
+
+def main():
