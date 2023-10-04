bc0f1837a3c04f4fcc9e856f9201b497c3c4e255
neisnei <neischalyruidiaz@gmail.com>
2023-10-04 00:46:55 +0000

    README.md

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        print("{:d}".format(value))
+        print("{:d}".format(value), end='')
         return True
-    except:
+    except ValueError:
+        return False
+    except TypeError:
         return False
