d57dccdbfe6333b63b74dc857721db8744e9b636
neisnei <neischalyruidiaz@gmail.com>
2023-10-12 03:34:53 +0000

    Square defines

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Module contains a square class"""
+"""This module contains a square class"""
 
 
 class Square:
-    """Class to represent a square."""
+    """A class to represent a square."""
     def __init__(self, size=0):
         """Initialize the square with a given size.
 
@@ -11,7 +11,7 @@ class Square:
         - size: The size of the square's sides.
         """
         if not isinstance(size, int):
-            raise TypeError("size must integer")
+            raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
