deda1fb137078443aeea61aef3e4afd1b741766c
neisnei <neischalyruidiaz@gmail.com>
2023-10-12 03:52:42 +0000

    Coordinates

---
+++ python-classes/6-square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""Module contains square class"""
+
+
+class Square:
+    """Class represent a square."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initialize the square with size and position."""
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Get the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square with validation."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """Get the position of the square."""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Set the position of the square with validation."""
+        if not isinstance(value, tuple) or len(value) != 2 or \
+           not all(isinstance(num, int) and num >= 0 for num in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """Calculate and return the area of the square."""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Print the square with the character '#' considering its position."""
+        if self.__size == 0:
+            print()
+            return
+
+        print("\n" * self.__position[1], end="")
+        for _ in range(self.__size):
+            print(" " * self.__position[0] + "#" * self.__size)
