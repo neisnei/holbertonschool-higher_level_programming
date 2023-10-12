27b2d42a7517573f61882a3e2a17933e0390c4c6
neisnei <neischalyruidiaz@gmail.com>
2023-10-12 03:30:26 +0000

    Square defines

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square"""
+
+
+class Square:
+    """This class defines a square"""
+
+    def __init__(self, size):
+        """Initializes the data"""
+        self.__size = size
