480d57ca89bc29d7f32fcc7d032c686d0850096c
neisnei <neischalyruidiaz@gmail.com>
2023-10-13 23:16:52 +0000

    integers addition

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,17 +1,29 @@
 #!/usr/bin/python3
 """
-Module
-add_integer: adds two integers and return
+    This module will have a function that will add 2 intergers.
+    First number will be a and second number passed will be b.
+    Returning the addition of both number.
 """
 
 
 def add_integer(a, b=98):
-    """Return sum of a + b
-    Args: a = int/float b = int/float
     """
-
-    if isinstance(a, (int, float)) is False:
+        Function that adds 2 intergers.
+        Check if a and b are intergers or floats.
+        If not a error will be raised.
+        Otherwise float will be casted to interger in order to
+        recreate add both.
+    """
+    if type(a) is not int and type(a) is not float:
         raise TypeError('a must be an integer')
-    if isinstance(b, (int, float)) is False:
+
+    if type(b) is not int and type(b) is not float:
         raise TypeError('b must be an integer')
-    return int(a + b)
+
+    if type(a) == float:
+        a = int(a)
+
+    if type(b) == float:
+        b = int(b)
+
+    return a + b
