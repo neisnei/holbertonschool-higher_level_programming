382ac2719329ce7de074853dc37f8ddf333ef0f2
neisnei <neischalyruidiaz@gmail.com>
2023-10-19 18:37:11 +0000

    Rectangle that inherits

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -4,10 +4,11 @@
 Module Rectangle
 """
 
-
 class Rectangle(BaseGeometry):
     """class that inherits from BaseGeometry"""
     def __init__(self, width, height):
         """Instantiation with width and height"""
         self.integer_validator('width', width)
         self.integer_validator('height', height)
+        self.__width = width
+        self.__height = height
