8209504f4d8a3a405b3220e5d8d5e92320c9ece0
neisnei <neischalyruidiaz@gmail.com>
2023-10-19 15:52:53 +0000

    returns the list

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+"""Define an lookup funtion."""
 def lookup(obj):
-"""Returns the list of available attributes and methods"""
+"""Returns the list of available attributes and miethods."""
     return (dir(obj))
