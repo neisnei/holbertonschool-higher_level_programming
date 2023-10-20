ab7fa79a8badaac62c75aa034a8d25e1122cb82d
neisnei <neischalyruidiaz@gmail.com>
2023-10-20 01:02:24 +0000

    inherits

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,6 +5,10 @@ Module inherits.
 """
 
 def inherits_from(obj, a_class):
-    """function returns True or False.
-    If the object an instance class that inherited the specified class"""
-    return issubclass(type(obj), a_class) and type(obj) is not a_class
+    """
+    True or False  if the object is an instance of a class
+    """
+    if isinstance(obj, a_class) and type(obj) is not a_class:
+        return True
+    else:
+        return False
