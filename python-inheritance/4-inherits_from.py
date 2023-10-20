bb8db6f3e08061d68dba8ce501e2e2776ce61a94
neisnei <neischalyruidiaz@gmail.com>
2023-10-20 01:05:50 +0000

    inherits

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """
-Module inherits.
+inherits_from module
 """
+
 
 def inherits_from(obj, a_class):
     """
-    True or False  if the object is an instance of a class
+    True if the object is an instance of a class that inherited
+    (directly or indirectly) from the specified class
+    False otheriwise
     """
     if isinstance(obj, a_class) and type(obj) is not a_class:
         return True
