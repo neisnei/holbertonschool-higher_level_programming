eac57c523ea740b0ea7b5c5755b199796cfd3169
neisnei <neischalyruidiaz@gmail.com>
2023-10-20 00:59:35 +0000

    kind class

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -6,5 +6,10 @@ Module kind of class.
 
 
 def is_kind_of_class(obj, a_class):
-    """Function returns True or False, the specified class."""
-    return isinstance (obj, a_class)
+    """
+    True or False of the object"
+    """
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
