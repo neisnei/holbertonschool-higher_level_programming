f204e6528a927c65ecf29ce881f1c32b225615ac
neisnei <neischalyruidiaz@gmail.com>
2023-10-20 12:35:36 +0000

    add item

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
-"""Module adds all arguments"""
+"""defines function to write JSON representatin of an object to file"""
 
 
-import sys
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
-try:
-    my_list = load_from_json_file("add_item.json")
-except:
-    my_list = []
-for i in sys.argv[1:]:
-    my_list.append(i)
-    save_to_json_file(my_list, "add_item.json")
+def save_to_json_file(my_obj, filename):
+    """write JSON representation of an object to a file"""
+    import json
+    with open(filename, 'w') as f:
+        f.write(json.dumps(my_obj, ensure_ascii=False))
