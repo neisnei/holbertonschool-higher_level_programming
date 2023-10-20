27fa136c472027a818bf7bdf0b263e025bad66a2
neisnei <neischalyruidiaz@gmail.com>
2023-10-20 12:21:00 +0000

    add item

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module adds all arguments"""
+
+
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+try:
+    my_list = load_from_json_file("add_item.json")
+except:
+    my_list = []
+for i in sys.argv[1:]:
+    my_list.append(i)
+    save_to_json_file(my_list, "add_item.json")
