d49eca3aa3b76f56b70963773d146f4082ae84aa
neisnei <neischalyruidiaz@gmail.com>
2023-11-25 15:20:35 +0000

    all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
 """
-This module is about the lists all states
+This module is the lists all states
 from database htbtn_0e_0_usa.
 """
 
 import MySQLdb
-import sys import argv
+from sys import argv
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     """
     Acess database and get states
     from database
@@ -18,7 +18,7 @@ if __name__ == "__main__":
             user=argv[1], passwd=argv[2], db=argv[3])
 
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states BY id ASC")
     rows = cursor.fetchall()
 
     for row in rows:
