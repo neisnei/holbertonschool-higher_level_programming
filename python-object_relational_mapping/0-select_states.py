e30eb7301ce11e7ec30e4787d2c65ef7d6b5b660
neisnei <neischalyruidiaz@gmail.com>
2023-11-25 15:42:21 +0000

    all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -9,16 +9,12 @@ import MySQLdb
 import sys
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(
-            host='localhost',
-            port=3306,
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3]
-            )
+    db = MySQLdb.connect(host='localhost, port=3306, user=sys.argv[1],
+    passwd=sys.argv[2], db=sys.argv[3])
+
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states BY id ASC")
     states = cur.fetchall()
 
-    for state in state:
-        print(state)
+    for row in rows:
+    print(row)
