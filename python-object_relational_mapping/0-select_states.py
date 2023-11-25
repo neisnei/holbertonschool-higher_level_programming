090f86c57152e18ae2ffee3354ed97580e41b348
neisnei <neischalyruidiaz@gmail.com>
2023-11-25 15:56:02 +0000

    all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -13,7 +13,8 @@ if __name__ == "__main__":
     password = sys.argv[2]
     database = sys.argv[3]
 
-    db = MySQLdb.connect(host="localhost",
+    db = MySQLdb.connect(
+            host="localhost",
             port=3306,
             user=argv[1],
             passwd=argv[2],
