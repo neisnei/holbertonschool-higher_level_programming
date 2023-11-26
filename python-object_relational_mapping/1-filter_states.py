d91b3a97faa5bf082d0408633c33a97e92b8e4bb
neisnei <neischalyruidiaz@gmail.com>
2023-11-26 20:45:58 +0000

    fix

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Module list all states with a name that starts with a
-upper character from database
+Module list all  states with a name that starts with a upper
+character from database hbtn_0e_0_usa.
 """
 
 
@@ -9,7 +9,12 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
+    """
+    Function lists all states with a name that starts with a
+    upper character from database hbtn_0e_0_usa.
+    """
 
+    # Connecting to a MySQL database.
     cnx = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -18,16 +23,23 @@ if __name__ == "__main__":
         passwd=sys.argv[2],
         database=sys.argv[3])
 
+    # Making cursor obj for execution.
     cur = cnx.cursor()
 
+    # Executing query.
     cur.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
 
+    # Obtaining query results.
     query_rows = cur.fetchall()
 
+    # Printing results.
     for row in query_rows:
         if row[1][0] == 'N':
             print(row)
 
+    # Close cursor.
     cur.close()
 
+    # Close connection to database.
     cnx.close()
