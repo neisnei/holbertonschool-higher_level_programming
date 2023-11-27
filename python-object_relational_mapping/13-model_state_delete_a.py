ac54a2deda0bc5bba4791470cba4651eefcdf482
neisnei <neischalyruidiaz@gmail.com>
2023-11-27 00:52:25 +0000

    Delete state

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """
-Module that deletes all State objects with a name
+Module deletes all State objects with a name
 containing the letter a from the database hbtn_0e_6_usa
 """
+
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
