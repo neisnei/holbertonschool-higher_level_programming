e305b49d48d28e411045e598fa08b94cbf9e3b99
neisnei <neischalyruidiaz@gmail.com>
2023-11-27 00:32:48 +0000

    Get a state

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,30 +1,26 @@
 #!/usr/bin/python3
 """
-Module state requested
+prints the State object with the name passed
+as argument from the database hbtn_0e_6_usa
 """
+
 import sys
-from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
 
-if __name__ == '__main__':
-    username = sys.argv[1]
-    password = sys.argv[2]
-    databases = sys.argv[3]
-    state_name = sys.argv[4]
-
-     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost:3306/{}'
-        .format(username, password, databases))
-
-    # custom session object class from db engine
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
-    # instance
     session = Session()
-    state = session.query(State).filter(State.name == state_name).first()
 
-    if not state:
+    found = False
+    for state in session.query(State):
+        if state.name == sys.argv[4]:
+            print("{}".format(state.id))
+            found = True
+            break
+    if found is False:
         print("Not found")
-
-    else:
-        print("{}".format(state.id))
