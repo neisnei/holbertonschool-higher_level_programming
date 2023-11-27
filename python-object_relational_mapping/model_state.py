9dd2f685cd1a1bb4d0516dfd4e4bed160b34e648
neisnei <neischalyruidiaz@gmail.com>
2023-11-27 00:01:59 +0000

    First state model

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """
-Module define the state class.
+Module define the State class.
 """
-
 
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
@@ -15,14 +14,12 @@ class state(Base):
     """
     Defining state class mapped to
     cities table in database hbtn_0e_14_usa.
-    """
 
-    Attributes:
         __tablename__: Name of table mapped to State class.
         id: Column representing primary key.
         name: Column representing name.
         state_id: Column representing foreign key to states table.
-    """
+        """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
